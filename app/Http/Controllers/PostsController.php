3be57c9abf3d4a40be2a4f1ad66229c0d5bfd19d
Ben Peng <3a732078@gm.student.ncut.edu.tw>
2021-01-17 15:25:25 +0800

    看過練習1-2，練習3使用controller 傳送data

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,12 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Post;
 
 class PostsController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $p = Post::orderBy('created_at','DESC')->get();
+        $data = ['posts'=>$p];
+        return view('posts.index',$data);
     }
 
     public function show($id)
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Post;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
